6cfa7aa9445a5f2ee9c9bb2fba7f625aff72cf91
Iain Cambridge <github@icambridge.me>
2013-01-21 20:20:21 +0000

    Added delete load balancer functionality

--- app/controllers/Loadbalancer.scala
+++ app/controllers/Loadbalancer.scala
@@ -14,7 +14,6 @@ import org.jclouds.rackspace.cloudloadbalancers.CloudLoadBalancersAsyncApi
 import org.jclouds.rackspace.cloudloadbalancers.domain.NodeRequest
 import org.jclouds.rest.RestContext
 import org.jclouds.rackspace.cloudloadbalancers.domain.internal.BaseNode._
-import org.jclouds.rackspace.cloudloadbalancers.domain.Node
 
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
@@ -40,9 +39,10 @@ object Loadbalancer extends Controller {
     val zone = parts(0)
     val id = parts(1).toInt
     val loadBalancer = loadBalance.getLoadBalancerMetadata(loadBalancerId)
-    val nova = loadBalance.getContext.unwrap.asInstanceOf[RestContext[CloudLoadBalancersApi, CloudLoadBalancersAsyncApi]]
+    val context =   loadBalance.getContext
+    val nova = context.unwrap.asInstanceOf[RestContext[CloudLoadBalancersApi, CloudLoadBalancersAsyncApi]]
     val lbApi =  nova.getApi.getLoadBalancerApiForZone(zone).get(id)
-    val nodes = lbApi.getNodes.toArray.map(_.asInstanceOf[Node])
+    val nodes = lbApi.getNodes.toArray.map(_.asInstanceOf[org.jclouds.rackspace.cloudloadbalancers.domain.Node])
 
 
     Ok(views.html.loadbalancer.view(loadBalancer, nodes))
@@ -57,6 +57,26 @@ object Loadbalancer extends Controller {
   def recreate(loadBalancerId: String) = TODO
 
 
-  def delete(loadBalancerId: String) = TODO
+  def delete(loadBalancerId: String) = Action { implicit request =>
+
+    val loadBalance = LoadBalance.getLoadBalancer
+    val loadBalancer = loadBalance.getLoadBalancerMetadata(loadBalancerId)
+
+
+    Ok(views.html.loadbalancer.delete(loadBalancer))
+
+  }
+
+
+  def deleteConfirm(loadBalancerId: String) = Action { implicit request =>
+
+
+    val loadBalance = LoadBalance.getLoadBalancer
+    loadBalance.destroyLoadBalancer(loadBalancerId)
+
+
+    Redirect(routes.Loadbalancer.index).flashing("success" -> "The load balancer was successfully deleted")
+
+  }
 
 }
