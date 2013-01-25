b4f25bea02e0054b4b17fc1f3711904ee5c7562d
Iain Cambridge <github@icambridge.me>
2013-01-25 14:49:10 +0000

    Added group create form

--- app/controllers/Group.scala
+++ app/controllers/Group.scala
@@ -2,12 +2,52 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import utils.{Compute, LoadBalance}
+
+
+import org.jclouds.loadbalancer.domain.LoadBalancerMetadata
 
 object Group extends Controller {
 
+  val form = Form(
+    mapping(
+      "id" -> optional(number),
+      "name" -> nonEmptyText,
+      "loadBalancerId" -> nonEmptyText,
+      "hardwareId" -> nonEmptyText,
+      "imageId" -> nonEmptyText
+    )(entity.Group.apply)(entity.Group.unapply))
+
   def index = TODO
 
-  def create = TODO
+  def create = Action { implicit request =>
+
+    val client = Compute.getClient
+    val loadBalance = LoadBalance.getLoadBalancer
+
+    val loadBalancers = loadBalance.listLoadBalancers.toArray.map({ item =>
+      val loadbalancer = item.asInstanceOf[LoadBalancerMetadata]
+      (loadbalancer.getId, loadbalancer.getName)
+    }).toSeq
+
+
+    val images = client.listImages.toArray.map({ item =>
+        val image = item.asInstanceOf[org.jclouds.compute.domain.Image]
+        (image.getId, image.getName)
+    }).toSeq
+
+
+    val hardwares = client.listHardwareProfiles.toArray.map({ item =>
+     val hardware = item.asInstanceOf[org.jclouds.compute.domain.Hardware]
+     (hardware.getId,hardware.getName)
+    }).toSeq
+
+
+    Ok(views.html.groups.create(form ,loadBalancers, images, hardwares))
+  }
 
   def createConfirm = TODO
 
---
+++ app/entity/Group.scala
@@ -0,0 +1,3 @@
+package entity
+
+case class Group(id: Option[Int], name: String, hardwareId: String, imageId: String, loadBalancerId: String)
